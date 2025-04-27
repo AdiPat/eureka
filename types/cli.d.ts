faf70c3018598e4001f605b662a19f0ff525167f
Aditya Patange <contact.adityapatange@gmail.com>
2025-04-27 05:39:51 +0530

    update types

---
+++ types/cli.d.ts
@@ -0,0 +1,6 @@
+import { Command } from "commander";
+/**
+ * Sets up the CLI using Commander.js.
+ * @returns {Command} The configured Command instance.
+ */
+export declare function setupCli(): Command;
---
+++ types/utils.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Load environment variables from a .env file.
+ * @param envPath - The path to the .env file. If not provided, it will look for a .env file in the current directory.
+ */
+export declare function loadEnv(envPath?: string): boolean;
