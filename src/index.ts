70716aaf9a73da8714b1a133054037eac8abf2c4
Aditya Patange <contact.adityapatange@gmail.com>
2025-04-27 05:38:22 +0530

    feat: banner printing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+import figlet from "figlet";
+import chalk from "chalk";
+import { setupCli } from "./cli";
+
+/**
+ * Prints a banner to the console.
+ */
+function printBanner() {
+  console.log(
+    figlet.textSync("Celesto!", {
+      font: "Standard",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+    })
+  );
+  console.log(
+    chalk.blueBright(
+      "Celesto™ is an AI agent that helps you generate startup ideas.\n" +
+        "Created by AdiPat, The Hackers Playbook. © All rights reserved.\n" +
+        "For queries and feedback, please reach out to us at: "
+    ) + chalk.yellowBright("contact.adityapatange@gmail.com")
+  );
+}
+
 /**
  * Main function to run the SynthLite CLI.
  */
